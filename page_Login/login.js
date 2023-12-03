5542e8921781788945b39c1644a950a6e2189cef
Mailson Rodrigues <mailsonrodriguesarts@gmail.com>
2023-12-03 13:44:32 -0300

    Criando tela reponsiva Login para Celular

---
+++ page_Login/login.js
@@ -0,0 +1,7 @@
+let mln = document.getElementById("buttonLogin")
+let divMLN = document.getElementById("divYellowMLN")
+ 
+mln.addEventListener('click', ()=>{
+    divMLN.innerHTML = "Seja bem Vindo ao Supercine"
+    alert("Parabéns por fazer parte do maior site de Cinema do mundo :) ")
+});
