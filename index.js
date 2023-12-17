20c8cc0a8284ab91f25153d9b61a1faf18b3e460
Mailson Rodrigues <mailsonrodriguesarts@gmail.com>
2023-12-17 12:20:58 -0300

    criando link do input index para pesquisa API

---
+++ index.js
@@ -0,0 +1,13 @@
+
+var el = document.querySelector("#searchHeader");
+el.addEventListener("click", function () {
+ 
+    let inputFilmesPesquisa = document.getElementById("inputFilmesPesquisa").value
+    location.href = "./page_Search/search.html"
+    let vlr = inputFilmesPesquisa
+ 
+    localStorage.setItem("tituloFilme", vlr);
+});
+
+
+
