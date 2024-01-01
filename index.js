31487945ee2de7e5959b28c65707c5f512950659
Mailson Rodrigues <mailsonrodriguesarts@gmail.com>
2024-01-01 10:12:24 -0300

    criado slide da pagina porincipal

--- index.js
+++ index.js
@@ -1,57 +1,48 @@
 
 var el = document.querySelector("#searchHeader");
 el.addEventListener("click", function () {
- 
+
     let inputFilmesPesquisa = document.getElementById("inputFilmesPesquisa").value
     location.href = "./page_Search/search.html"
     let vlr = inputFilmesPesquisa
- 
+
     localStorage.setItem("tituloFilme", vlr);
 });
 
 let carPrincipalImg = document.getElementById("carPrincipalImg")
 
-function iniciarSlide(){
-    setTimeout(() => {
-        carPrincipalImg.style.backgroundImage = `url(../src/img/slideImages/01.png)`
-    }, 5000)
-
-    setTimeout(() => {
-        carPrincipalImg.style.backgroundImage = `url(../src/img/slideImages/02.png)`
-    }, 10000)
-
-    setTimeout(() => {
-        carPrincipalImg.style.backgroundImage = `url(../src/img/slideImages/03.png)`
-    }, 15000)
-
-    setTimeout(() => {
-        carPrincipalImg.style.backgroundImage = `url(../src/img/slideImages/04.png)`
-    }, 20000)
-}
-iniciarSlide()
-
-function repetirSlide(){
-    setTimeout(() => {
-        carPrincipalImg.style.backgroundImage = `url(../src/img/slideImages/01.png)`
-    }, 5000)
-
-    setTimeout(() => {
-        carPrincipalImg.style.backgroundImage = `url(../src/img/slideImages/02.png)`
-    }, 10000)
-
-    setTimeout(() => {
-        carPrincipalImg.style.backgroundImage = `url(../src/img/slideImages/03.png)`
-    }, 15000)
-
-    setTimeout(() => {
-        carPrincipalImg.style.backgroundImage = `url(../src/img/slideImages/04.png)`
-    }, 20000)
- 
 
 
-  
+
+function repetirSlide() {
+    
+    const imgSlides = ["01", "02", "03", "04"]
+
+    let contador = 0
+    setInterval(() =>{
+   
+        if(contador === 4){
+            carPrincipalImg.style.backgroundImage = `url(../src/img/cardPrincipal.png)`;
+            contador = 0
+        }
+        else{
+            console.log(contador)
+            carPrincipalImg.style.backgroundImage = `url(../src/img/slideImages/${imgSlides[contador]}.png)`;
+            contador++
+
+        }
+    }, 4000);
+
+
+
+
+
+
+
+
+
 }
 
 
-setInterval(repetirSlide,25000)
+repetirSlide()
 
