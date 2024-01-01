dbc3d34f010a65d96305ac32bc7e901fc72f9884
Mailson Rodrigues <mailsonrodriguesarts@gmail.com>
2023-12-31 22:17:21 -0300

    Adicionado localStorage para capas e descricao

--- index.js
+++ index.js
@@ -8,3 +8,50 @@ el.addEventListener("click", function () {
  
     localStorage.setItem("tituloFilme", vlr);
 });
+
+let carPrincipalImg = document.getElementById("carPrincipalImg")
+
+function iniciarSlide(){
+    setTimeout(() => {
+        carPrincipalImg.style.backgroundImage = `url(../src/img/slideImages/01.png)`
+    }, 5000)
+
+    setTimeout(() => {
+        carPrincipalImg.style.backgroundImage = `url(../src/img/slideImages/02.png)`
+    }, 10000)
+
+    setTimeout(() => {
+        carPrincipalImg.style.backgroundImage = `url(../src/img/slideImages/03.png)`
+    }, 15000)
+
+    setTimeout(() => {
+        carPrincipalImg.style.backgroundImage = `url(../src/img/slideImages/04.png)`
+    }, 20000)
+}
+iniciarSlide()
+
+function repetirSlide(){
+    setTimeout(() => {
+        carPrincipalImg.style.backgroundImage = `url(../src/img/slideImages/01.png)`
+    }, 5000)
+
+    setTimeout(() => {
+        carPrincipalImg.style.backgroundImage = `url(../src/img/slideImages/02.png)`
+    }, 10000)
+
+    setTimeout(() => {
+        carPrincipalImg.style.backgroundImage = `url(../src/img/slideImages/03.png)`
+    }, 15000)
+
+    setTimeout(() => {
+        carPrincipalImg.style.backgroundImage = `url(../src/img/slideImages/04.png)`
+    }, 20000)
+ 
+
+
+  
+}
+
+
+setInterval(repetirSlide,25000)
+
